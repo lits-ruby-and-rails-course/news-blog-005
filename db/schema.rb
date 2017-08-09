0ec99e668943799b93b72fc947d8fff47919cbdb
Mykhaylo Panchuk <websevens@gmail.com>
2017-08-09 09:35:07 +0300

    Improved parser, added Image to Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803162358) do
+ActiveRecord::Schema.define(version: 20170808170410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "title"
+    t.string "source"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "origin_url"
+    t.index ["origin_url"], name: "index_posts_on_origin_url"
   end
 
 end
